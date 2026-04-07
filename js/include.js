e368fdac7b0514b30a4ff56d13c9032d1bcc5140
Arin <dprus18@gmail.com>
2026-04-07 05:01:21 +0300

    fixes

--- js/include.js
+++ js/include.js
@@ -6,5 +6,5 @@ async function loadComponent(id, file) {
     
 }
 
-loadComponent("header", "components/header.html");
-loadComponent("footer", "components/footer.html");
+loadComponent("header", "../components/header.html");
+loadComponent("footer", "../components/footer.html");
