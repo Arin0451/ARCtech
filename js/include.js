80f1a5123b5474b9d31792132eb0adc60bb1df51
Arin <dprus18@gmail.com>
2026-04-07 05:11:06 +0300

    fixes

--- js/include.js
+++ js/include.js
@@ -2,9 +2,9 @@ async function loadComponent(id, file) {
     const response = await fetch(file);
     const text = await response.text();
     document.getElementById(id).innerHTML = text;
-    
+    console.log('подтянулось успешно');
     
 }
 
-loadComponent("header", "/arctech/components/header.html");
-loadComponent("footer", "/arctech/components/footer.html");
+loadComponent("header", "../components/header.html");
+loadComponent("footer", "../components/footer.html");
